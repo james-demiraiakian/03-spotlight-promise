90bd4c6291392be4d44b277ef573de20ddb9cd26
James Demiraiakian <james.armen@gmail.com>
2022-01-25 12:10:54 -0800

    did the thing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import { useEffect } from 'react';
+import {
+  asyncAIC,
+  asyncCat,
+  asyncPonball,
+  asyncResp,
+  fetchResp,
+  thenAIC,
+  thenCat,
+  thenPonball,
+} from './services/promise-me';
+
 export default function App() {
+  useEffect(() => {
+    fetchResp();
+    asyncResp();
+    thenPonball();
+    asyncPonball();
+    thenAIC();
+    asyncAIC();
+    thenCat();
+    asyncCat();
+  });
+
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,23 +1,67 @@
 /**
- * TODO:
+ * TODO: Write a fetch request an API using .then AND async/await
+ * An API of APIs
  */
+export function fetchResp() {
+  fetch('https://api.publicapis.org/random')
+    .then((response) => response.json())
+    .then((data) => console.log('API of APIs .then', data));
+}
+
+export async function asyncResp() {
+  const resp = await fetch('https://api.publicapis.org/entries');
+  const data = await resp.json();
+  console.log('API of APIs async/await-', data);
+  return resp;
+}
+
+/**
+ * TODO: Ponball DB calls
+ */
+export function thenPonball() {
+  fetch('https://ponball.herokuapp.com/api/v1/machines')
+    .then((resp) => resp.json())
+    .then((data) => console.log('.then ponball', data));
+}
+
+export async function asyncPonball() {
+  const resp = await fetch('https://ponball.herokuapp.com/api/v1/machines');
+  const data = await resp.json();
+  console.log('async ponball', data);
+}
+/**
+ * TODO: Art institute of Chicago
+ */
+export function thenAIC() {
+  fetch('https://api.artic.edu/api/v1/artworks')
+    .then((resp) => resp.json())
+    .then((data) => console.log('AIC .then', data));
+}
+
+export async function asyncAIC() {
+  const resp = await fetch('https://api.artic.edu/api/v1/artworks');
+  const data = await resp.json();
+  console.log('async AIC', data);
+}
 
 /**
  * TODO:
  */
 
 /**
- * TODO:
+ * TODO: cat api
  */
+export function thenCat() {
+  fetch('https://thatcopy.pw/catapi/rest/')
+    .then((resp) => resp.json())
+    .then((data) => console.log('Cat .then', data));
+}
 
-/**
- * TODO:
- */
-
-/**
- * TODO:
- */
-
+export async function asyncCat() {
+  const resp = await fetch('https://thatcopy.pw/catapi/rest/');
+  const data = await resp.json();
+  console.log('Cat async', data);
+}
 /**
  * TODO:
  */
